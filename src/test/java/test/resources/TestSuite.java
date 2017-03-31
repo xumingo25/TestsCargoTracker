98e9c6ca4091ae3a5b62854460ee58cde463de82
Xumingo25 <domingo.saavedra.saavedra@gmail.com>
2017-03-31 11:32:08 -0300

    Update

--- src/test/java/test/resources/TestSuite.java
+++ src/test/java/test/resources/TestSuite.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ClonTest_reporteHTML.class,ClonTest_reporteLib.class
+@SuiteClasses({ ClonTest_reporteHTML.class,ClonTest_reporteLib.class,AboutFeature.class,BookFeature.class,
+				TrackFeature.class
 	
 })
 public class TestSuite {
